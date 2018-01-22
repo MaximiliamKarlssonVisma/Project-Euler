21190ec5ad4e954b4d2dbead93ddd11f9492a813
MaximiliamKarlssonVisma <33834427+MaximiliamKarlssonVisma@users.noreply.github.com>
2018-01-22 10:25:37 +0100

    Made the program shorter
    Removed the array and made the program more slim.

--- 1 - Multiples of 3 and 5.rb
+++ 1 - Multiples of 3 and 5.rb
@@ -1,13 +1,10 @@
-## If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-## Find the sum of all the multiples of 3 or 5 below 1000.
+/
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+/
+
 def sum_multi_numbers(max_number)
-	list_of_numbers = (1..max_number-1).to_a
-	sum = 0
-	list_of_numbers.each do |number|
-		if number % 3 == 0 or number % 5 == 0
-			sum += number
-		end
-	end
-	return sum
-end
+	sum = 0; (max_number - 1).times{|i| sum += i if i % 3 == 0 || i % 5 == 0}; return sum
+end 
+
 p sum_multi_numbers(1000)
